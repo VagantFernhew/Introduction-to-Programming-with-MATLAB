5fe666893ea8a73b0676f2d65ee8497d1f9e989a
VagantFernhew <vagant.20@gmail.com>
2017-07-29 02:53:18 +0500

    Create circle.m

---
+++ hw3/circle.m
@@ -0,0 +1,10 @@
+function [area, circumf] = circle(r)
+% this function takes radius of circle as input and
+% return area & circumfrance of circle,
+% ================================
+%compute area of circle
+ area = pi*r*r;
+% compute circumfrance of circle
+ circumf = 2*pi*r;
+
+end
